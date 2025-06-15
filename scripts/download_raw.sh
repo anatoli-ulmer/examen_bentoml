70ebf5e0e29dc721d365dffa914f93aa80e5327f
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2025-06-15 13:42:12 +0000

    fix broken download link and output path

--- scripts/download_raw.sh
+++ scripts/download_raw.sh
@@ -4,4 +4,4 @@
 mkdir -p data/raw
 
 # Download the file
-curl -o data/raw/admissions.csv https://datascientest.s3-eu-west-1.amazonaws.com/examen_bentoml/admissions.csv
+curl -o data/raw/admission.csv https://assets-datascientest.s3.eu-west-1.amazonaws.com/MLOPS/bentoml/admission.csv
